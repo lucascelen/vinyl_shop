75109a5d3b9cf2fa8291060364d19a03fae4fba7
Lucas Celen <r0710760@student.thomasmore.be>
2020-11-26 14:33:07 +0100

    Custom error pages

--- routes/web.php
+++ routes/web.php
@@ -46,5 +46,13 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
     Route::get('records', 'Admin\RecordController@index');
 });
 
+Route::middleware(['auth'])->prefix('user')->group(function () {
+    Route::redirect('/', '/user/profile');
+    Route::get('profile', 'User\ProfileController@edit');
+    Route::post('profile', 'User\ProfileController@update');
+    Route::get('password', 'User\PasswordController@edit');
+    Route::post('password', 'User\PasswordController@update');
+});
+
 
 
